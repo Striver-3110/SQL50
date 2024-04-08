4df765820f6dc9457ba6a5f0eefc49a5bc3edd2d
Striver-3110 <21csjay067@ldce.ac.in>
2024-04-08 18:42:17 +0530

    The Number of Employees Which Report to Each Employee

---
+++ 33Primary Department for Each Employee.sql
@@ -0,0 +1,60 @@
+-- Approach 1
+-- CTE Approach
+    -- WITH
+    --     Filtered AS(
+    --         SELECT
+    --             * 
+    --         FROM
+    --             Employee
+    --         WHERE
+    --             primary_flag = 'Y'
+
+    --     ),
+        
+    --     TableWithNull AS(
+    --         SELECT  
+    --             employee_id,
+    --             IF(COUNT(department_id) = 1,department_id,
+    --             (
+    --                 SELECT 
+    --                     Filtered.department_id 
+    --                 FROM
+    --                     Filtered
+    --                 WHERE
+    --                     Employee.employee_id = Filtered.employee_id) 
+    --             )AS department_id
+    --         FROM
+    --             Employee
+    --         GROUP BY
+    --             employee_id
+
+    --     )
+
+    -- SELECT
+    --     * 
+    -- FROM
+    --     TableWithNull
+    -- WHERE
+    --     department_id IS NOT NULL;
+    
+-- Approach-2
+-- using union
+    SELECT
+        employee_id,
+        department_id
+    FROM
+        Employee
+    WHERE
+        primary_flag = 'Y'
+
+    UNION
+
+    SELECT
+        employee_id,
+        department_id
+    FROM
+        Employee
+    GROUP BY
+        employee_id
+    HAVING 
+        COUNT(department_id) = 1
