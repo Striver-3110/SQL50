6bfca3816543afdc7bf88adc65d309b45e796336
Striver-3110 <21csjay067@ldce.ac.in>
2024-04-10 18:47:17 +0530

    Last Person to Fit in the Bus

---
+++ 37Last Person to Fit in the Bus.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT
+    person_name
+FROM
+    (
+        SELECT
+            person_name,
+            SUM(weight) OVER (ORDER BY Turn) AS cumulativeWeight
+        FROM
+            Queue
+    )AS CTE
+WHERE
+    cumulativeWeight <= 1000
+ORDER BY
+    cumulativeWeight DESC
+LIMIT 1;
