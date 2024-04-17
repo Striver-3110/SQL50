72a678b26b899df37283c76eed7df8312a57530a
Striver-3110 <21csjay067@ldce.ac.in>
2024-04-17 19:00:12 +0530

    Patients With a Condition

---
+++ 41Patients With a Condition.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+
+SELECT
+    *
+FROM
+    Patients
+WHERE
+    conditions 
+    LIKE
+    "DIAB1%"
+    OR
+    conditions 
+    LIKE
+    "% DIAB1%"
+    
