890a8df98b7bb70ec6f7f50dbba824efbccc3460
Striver-3110 <21csjay067@ldce.ac.in>
2024-06-15 18:24:26 +0530

    15NotBoaringMovies

---
+++ 15Not Boring Movies.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT 
+    *
+FROM 
+    Cinema
+WHERE
+    MOD(id,2) = 1
+    -- id % 2 = 1 
+    AND
+    description <> "boring"
+ORDER BY
+    rating DESC;
